5f51d3431e1cdab3c57580c79783a3d66d26b71f
Emiliano ZERBINO <emilianozerbino@yahoo.com.ar>
2023-05-05 23:30:28 -0300

    corregi el crossorigin

--- src/main/java/com/portfolio/portfolio/WebConfig.java
+++ src/main/java/com/portfolio/portfolio/WebConfig.java
@@ -1,34 +1,23 @@
 package com.portfolio.portfolio;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
- 
-
-/**
-
- * Clase que habilita CORS
-
- * @author YOProgramo
-
- */
-
-@EnableWebMvc
 
 @Configuration
+public class WebConfig {
 
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**");
-
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Permitir cualquier origen
+        configuration.addAllowedMethod("*"); // Permitir cualquier método (GET, POST, etc.)
+        configuration.addAllowedHeader("*"); // Permitir cualquier encabezado
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplicar la configuración a todas las rutas
+        return source;
     }
-
-   
-
 }
